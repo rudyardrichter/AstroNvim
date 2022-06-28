a554a6519946ef156d9e1a5f1c3baca3d7fdb79c
Rudyard Richter <rudyardrichter@uchicago.edu>
2022-06-28 09:27:18 -0500

    Tweak core mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -45,17 +45,6 @@ else
   maps.n["<leader>c"] = { "<cmd>bdelete<cr>", desc = "Close window" }
 end
 
--- Navigate buffers
-if is_available "bufferline.nvim" then
-  maps.n["<S-l>"] = { "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer tab" }
-  maps.n["<S-h>"] = { "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer tab" }
-  maps.n[">b"] = { "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer tab right" }
-  maps.n["<b"] = { "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer tab left" }
-else
-  maps.n["<S-l>"] = { "<cmd>bnext<cr>", desc = "Next buffer" }
-  maps.n["<S-h>"] = { "<cmd>bprevious<cr>", desc = "Previous buffer" }
-end
-
 -- Comment
 if is_available "Comment.nvim" then
   maps.n["<leader>/"] = {
@@ -152,69 +141,10 @@ if is_available "nvim-lsp-installer" then
   maps.n["<leader>lI"] = { "<cmd>LspInstallInfo<cr>", desc = "LSP installer" }
 end
 
--- Smart Splits
-if is_available "smart-splits.nvim" then
-  -- Better window navigation
-  maps.n["<C-h>"] = {
-    function()
-      require("smart-splits").move_cursor_left()
-    end,
-    desc = "Move to left split",
-  }
-  maps.n["<C-j>"] = {
-    function()
-      require("smart-splits").move_cursor_down()
-    end,
-    desc = "Move to below split",
-  }
-  maps.n["<C-k>"] = {
-    function()
-      require("smart-splits").move_cursor_up()
-    end,
-    desc = "Move to above split",
-  }
-  maps.n["<C-l>"] = {
-    function()
-      require("smart-splits").move_cursor_right()
-    end,
-    desc = "Move to right split",
-  }
-
-  -- Resize with arrows
-  maps.n["<C-Up>"] = {
-    function()
-      require("smart-splits").resize_up()
-    end,
-    desc = "Resize split up",
-  }
-  maps.n["<C-Down>"] = {
-    function()
-      require("smart-splits").resize_down()
-    end,
-    desc = "Resize split down",
-  }
-  maps.n["<C-Left>"] = {
-    function()
-      require("smart-splits").resize_left()
-    end,
-    desc = "Resize split left",
-  }
-  maps.n["<C-Right>"] = {
-    function()
-      require("smart-splits").resize_right()
-    end,
-    desc = "Resize split right",
-  }
-else
-  maps.n["<C-h>"] = { "<C-w>h", desc = "Move to left split" }
-  maps.n["<C-j>"] = { "<C-w>j", desc = "Move to below split" }
-  maps.n["<C-k>"] = { "<C-w>k", desc = "Move to above split" }
-  maps.n["<C-l>"] = { "<C-w>l", desc = "Move to right split" }
-  maps.n["<C-Up>"] = { "<cmd>resize -2<CR>", desc = "Resize split up" }
-  maps.n["<C-Down>"] = { "<cmd>resize +2<CR>", desc = "Resize split down" }
-  maps.n["<C-Left>"] = { "<cmd>vertical resize -2<CR>", desc = "Resize split left" }
-  maps.n["<C-Right>"] = { "<cmd>vertical resize +2<CR>", desc = "Resize split right" }
-end
+maps.n["<C-Up>"] = { "<cmd>resize -2<CR>", desc = "Resize split up" }
+maps.n["<C-Down>"] = { "<cmd>resize +2<CR>", desc = "Resize split down" }
+maps.n["<C-Left>"] = { "<cmd>vertical resize -2<CR>", desc = "Resize split left" }
+maps.n["<C-Right>"] = { "<cmd>vertical resize +2<CR>", desc = "Resize split right" }
 
 -- SymbolsOutline
 if is_available "aerial.nvim" then
