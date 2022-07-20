7819601d28d427bd41e42a8a7d0809f81a3e26f0
Rudyard Richter <richter3141@gmail.com>
2022-07-20 15:08:21 -0600

    Add pyright config to load venvs

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,3 +1,43 @@
+local find_cmd = function(cmd, prefixes, start_from, stop_at)
+  local path = require("lspconfig/util").path
+
+  if type(prefixes) == "string" then
+    prefixes = { prefixes }
+  end
+
+  local found
+  for _, prefix in ipairs(prefixes) do
+    local full_cmd = prefix and path.join(prefix, cmd) or cmd
+    local possibility
+
+    -- if start_from is a dir, test it first since transverse will start from its parent
+    if start_from and path.is_dir(start_from) then
+      possibility = path.join(start_from, full_cmd)
+      if vim.fn.executable(possibility) > 0 then
+        found = possibility
+        break
+      end
+    end
+
+    path.traverse_parents(start_from, function(dir)
+      possibility = path.join(dir, full_cmd)
+      if vim.fn.executable(possibility) > 0 then
+        found = possibility
+        return true
+      end
+      -- use cwd as a stopping point to avoid scanning the entire file system
+      if stop_at and dir == stop_at then
+        return true
+      end
+    end)
+
+    if found ~= nil then
+      break
+    end
+  end
+  return found or cmd
+end
+
 local palette = require("gruvbox.palette")
 require("gruvbox").setup({
   inverse = false,
@@ -159,9 +199,9 @@ return {
   -- Extend LSP configuration
   lsp = {
     -- enable servers that you already have installed without lsp-installer
-    servers = {
-      -- "pyright"
-    },
+    -- servers = {
+    --   "pyright",
+    -- },
     -- add to the server on_attach function
     -- on_attach = function(client, bufnr)
     -- end,
@@ -185,15 +225,15 @@ return {
       --     },
       --   },
       -- },
-      pyright = {
-        settings = {
-          python = {
-            analysis = {
-              typeCheckingMode = "on",
-            }
-          }
-        }
-      }
+      -- pyright = {
+      --   settings = {
+      --     python = {
+      --       analysis = {
+      --         typeCheckingMode = "on",
+      --       }
+      --     }
+      --   }
+      -- }
     },
   },
 
@@ -228,5 +268,22 @@ return {
         }
       }
     }
+    -- require("py_lsp").setup{}
+    local nvim_lsp = require("lspconfig")
+    local lsp_util = require("lspconfig/util")
+    nvim_lsp.pyright.setup({
+      before_init = function(_, config)
+        local p
+        if vim.env.VIRTUAL_ENV then
+          p = lsp_util.path.join(vim.env.VIRTUAL_ENV, "bin", "python3")
+        else
+          p = find_cmd("python3", ".venv/bin", config.root_dir)
+        end
+        config.settings.python.pythonPath = p
+      end,
+      settings = {
+        disableOrganizeImports = true,
+      },
+    })
   end,
 }
